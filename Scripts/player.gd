c74c3f263947fe48afc891c46f83d72705954b40
Bernat-dev <aymerich.medina.bernat@alumnes.fundaciocic.org>
2025-04-04 13:29:46 +0200

    Dia3
    He canviat el codi i he posat noves textures

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var velocitat:= 500
 var direccio := Vector2.ZERO
 var gravetat = Vector2.DOWN * 988
-var salt := -1000
+var salt := -500
 var max_salt := 2
 var salts_disponibles := max_salt
 
@@ -14,14 +14,19 @@ func _process(delta: float) -> void:
 	var dx = Input.get_axis("moure_esquerra","moure_dreta")
 	velocity.x=0
 	
-	velocity.x = dx * velocitat * Vector2.RIGHT
+	velocity.x += dx * velocitat
 
 	if Input.is_action_just_pressed("salt") and salts_disponibles > 0:
-		velocity.y = salt
-		salts_disponibles = 1
-
+		velocity.y += salt
+		salts_disponibles -= 1
+	
 	move_and_slide()
 	
 	velocity += gravetat * delta
 	if is_on_floor():
 		salts_disponibles = 2
+		
+	if velocity.x > 0:
+		$player_sprite.flip_h = true
+	else:
+		$player_sprite.flip_h = false
